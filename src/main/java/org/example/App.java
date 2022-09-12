b19e6f670aa6bd1dd5aa4cb81acbdb335f302537
Majed90alfaifi <99211143+Majed90alfaifi@users.noreply.github.com>
2022-09-12 18:08:53 +0300

    Add files via upload

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,13 @@
+package org.example;
+
+
+public class App{
+    public static void main(String[]args){
+        Order o1 = new Order();
+        //Order o2 = new Order();
+
+        Shipment sh1 = new Shipment("Ahmed",
+                "248 NE. Pleasant St. Niceville, FL 32578",
+                "123-477-0001");
+    }
+}
---
+++ src/test/java/org/example/LoggerTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class LoggerTest  {
+
+    @Test
+    public void shouldBeIdenticals()
+    {
+        Logger log1 =  Logger.getInstance();
+        Logger log2 =  Logger.getInstance();
+        assertEquals(log1, log2);
+    }
+}
